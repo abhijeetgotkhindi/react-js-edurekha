b3422ac14551feef3933524fa7ae056407be9c75
Abhijeet Gotkhindi <abhijeet.gotkhindi@gmail.com>
2024-03-15 22:23:38 +0530

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import enquiriesReducer from './enqiries';
 
 const rootReducer = combineReducers({
-    enqiries: enquiriesReducer
+    enquiriesReducer: enquiriesReducer
 });
 
 export default rootReducer;
